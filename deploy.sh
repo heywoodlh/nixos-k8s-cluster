11ab2eb951f0796bc2276c44c3966cdce1f5d257
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-03-10 10:02:31 -0700

    updated nfs-client provisioner
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,12 @@ kubectl get ns/argocd &>/dev/null || kubectl create ns argocd
 # Deploy argo-cd
 helm dep update charts/argo-cd
 helm list | grep -q argo-cd || helm upgrade -n argocd --install argo-cd charts/argo-cd/ -f ./charts/argo-cd/values.yaml
+
 # Deploy bootstrap-app (wait 45 seconds for argo to deploy)
 helm dep update bootstrap-apps
-helm list | grep -q bootstrap-app &> /dev/null || echo 'waiting 45 seconds for argo to deploy' && sleep 45 && helm upgrade -n argocd --install bootstrap-app apps/ -f ./apps/values.yaml
+helm list | grep -q bootstrap-app &> /dev/null || echo 'waiting 45 seconds for argo to deploy' && sleep 45 
+argo_args="--port-forward --port-forward-namespace argocd --plaintext"
+# Login with argocd CLI
+argocd login --username admin --password $(kubectl get secret -n argocd argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d) ${argo_args}
+# Create argo bootstrap-app
+argocd app list ${argo_args} | grep -q bootstrap-app || argocd app create bootstrap-app --project security --repo https://github.com/heywoodlh/k8s-security.git
